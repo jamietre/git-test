6f5ee6fc1c8baf853344dd5ba870879e0a11f89f
James Treworgy <jamietre@gmail.com>
2016-10-07 12:36:55 -0400

    Force line endings for cli.js to LF on publish

---
+++ build/fix-endings.js
@@ -0,0 +1,10 @@
+// global npm modules don't work on *nix with windows line endings on bin fileName
+
+var eol = require('eol')
+var path = require('path')
+var fs = require('fs')
+
+var fileName = path.resolve(__dirname, '../lib/cli.js');
+var data = fs.readFileSync(fileName, 'utf-8');
+
+fs.writeFileSync(fileName, eol.lf(data), 'utf-8');
